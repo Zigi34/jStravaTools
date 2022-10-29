53c5bcd49343524ecbfbf0bbe0859344de2fc53e
Zdeněk Gold <gol128@vsb.cz>
2022-10-30 00:03:06 +0200

    Initial commit

---
+++ src/main/java/org/zigi/jstravatool/JStravaToolApplication.java
@@ -0,0 +1,13 @@
+package org.zigi.jstravatool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JStravaToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JStravaToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/zigi/jstravatool/JStravaToolApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zigi.jstravatool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JStravaToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
