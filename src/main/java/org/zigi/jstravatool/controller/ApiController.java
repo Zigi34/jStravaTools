55a275f989f35a1a05b98577c64f1d2455761429
Zdeněk Gold <gol128@vsb.cz>
2022-10-30 01:59:02 +0200

    Logged user #1 - get logged athlete info in json

--- src/main/java/org/zigi/jstravatool/controller/ApiController.java
+++ src/main/java/org/zigi/jstravatool/controller/ApiController.java
@@ -7,6 +7,8 @@ import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.methods.RequestBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,12 +25,14 @@ public class ApiController {
 
     private final ApplicationConfiguration applicationConfiguration;
 
+    private final Logger LOG = LogManager.getLogger();
+
     public ApiController(ApplicationConfiguration applicationConfiguration) {
         this.applicationConfiguration = applicationConfiguration;
     }
 
     @GetMapping(name = "/")
-    public Athlete getLoggerAthlete() throws IOException {
+    public Athlete getLoggerAthlete() {
         try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
             HttpUriRequest request = RequestBuilder.get()
                     .setUri("https://www.strava.com/api/v3/athlete")
@@ -39,7 +43,12 @@ public class ApiController {
             try (InputStream stream = response.getEntity().getContent()) {
                 ObjectMapper mapper = new ObjectMapper();
                 return mapper.readValue(stream, Athlete.class);
+            } catch(Exception e) {
+                LOG.error("Fail read response stream", e);
             }
+        } catch(Exception e) {
+            LOG.error("Fail request", e);
         }
+        return null;
     }
 }
