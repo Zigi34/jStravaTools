feff8f0389f839b0767514ea0143e70c91eabaf5
Zdeněk Gold <gol128@vsb.cz>
2022-10-30 14:58:07 +0100

    Logged user #1 - deserialize all properties

--- src/main/java/org/zigi/jstravatool/util/Constants.java
+++ src/main/java/org/zigi/jstravatool/util/Constants.java
@@ -2,10 +2,12 @@ package org.zigi.jstravatool.util;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class Constants {
     public static final ObjectMapper MAPPER = new ObjectMapper();
     static {
         MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        MAPPER.registerModule(new JavaTimeModule());
     }
 }
